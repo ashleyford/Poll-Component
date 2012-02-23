8a9f5fe3ca1e0128566a952b7297098bb07c313b
Mbeng Ngassa <bearphace@gmail.com>
2012-02-23 11:38:49 +0000

    done line
    added comment

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ $("document").ready(function () {
     
 	function getPoll(){
 	
+	// done by bearphace.
 	
 	$.getJSON('http://papermashup.com/sproutComponents/poll/get_poll.php?pollid=<?php echo $_GET['pollid'];?>&type=getpoll&callback=?', function (data) {
 		
